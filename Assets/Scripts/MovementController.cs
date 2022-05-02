3b00b293febb5dd799d97ba50884b80a9433498d
physige <niaar2004@gmail.com>
2022-05-01 21:56:03 -0400

    added grapple feature

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,12 +6,24 @@ public class MovementController : MonoBehaviour
 {
     // gets character controller
     public CharacterController controller;
+    
+    // test object for raycast position
+    public Transform grappleObject;
+
+    // gets player camera
+    public Camera playerCamera;
 
     // player movement speed
     public float speed = 12f;
 
+    // player grapple speed
+    public float grappleSpeed = 24f;
+
     // gravity constant
     public float gravity = -9.81f;
+
+    // drag to slow down player momentum after grapple
+    public float drag = -8;
     
     // stores current speed of player
     private Vector3 velocity;
@@ -27,6 +39,13 @@ public class MovementController : MonoBehaviour
     public LayerMask groundMask;
     // stores whether or not player is touching the ground
     private bool isOnGround;
+
+    // stores the point at which the grapple hook landed
+    private Vector3 grapplePoint;
+
+    // stores what state the player is in i.e. normal movement or doing a grapple shot
+    private string playerState = "move";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +55,30 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (playerState == "grapple") {
+            grappleMovement();
+        } else {
+            playerMovement();
+        }
+
+        // if E is pressed, a grapple is shot
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit raycastHit)) {
+                // raycastHit is whatever the raycast hits
+                
+                // moves test object to where we are looking
+                grappleObject.position = raycastHit.point;
+
+                // stores position of grapple
+                grapplePoint = raycastHit.point;
+                
+                // prevents player controlled movement
+                playerState = "grapple";
+            }
+        }
+    }
+
+    private void playerMovement() {
         // checks if player is touching the ground
         isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -43,12 +86,44 @@ public class MovementController : MonoBehaviour
         if (isOnGround && velocity.y < 0) {
             // resets the player falling velocity (it's -2 because the condition may be called before fully touching ground)
             velocity.y = -2f;
+
+            // resets the player's x, z velocity from the grapple if the player touches the ground
+            velocity.x = 0f;
+            velocity.z = 0f;
+        }
+
+        // dampens player momentum from grapple
+        if (velocity.x != 0 || velocity.z != 0) {
+            // slows player's x velocity depending on initial direction
+            if (velocity.x > 0.1) {
+                velocity.x += drag * Time.deltaTime;
+            } else if (velocity.x < -0.1) {
+                velocity.x -= drag * Time.deltaTime;
+            } else {
+                velocity.x = 0;
+            }
+
+            // slows player's z velocity depending on initial direction
+            if (velocity.z > 0.1) {
+                velocity.z += drag * Time.deltaTime;
+            } else if (velocity.z < -0.1) {
+                velocity.z -= drag * Time.deltaTime;
+            } else {
+                velocity.z = 0;
+            }
         }
 
         // gets A D input (if D, x is 1; if A, x is -1)
         float x = Input.GetAxis("Horizontal");
         // gets W S input (if W, z is 1; if A, z is -1)
         float z = Input.GetAxis("Vertical");
+
+        // gets shift key and handles sprint
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            speed = 24f;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            speed = 12f;
+        }
 
         // moves player in respect to global direction
         // Vector3 move = new Vector3(x, 0f, z);
@@ -70,5 +145,25 @@ public class MovementController : MonoBehaviour
 
         // moves player's position based on velocity and makes player fall
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void grappleMovement() {
+        // calculates direction of player grapple
+        Vector3 grappleDirection = (grapplePoint - transform.position).normalized;
+        
+        // moves player in direction of grapple
+        controller.Move(grappleDirection * grappleSpeed * Time.deltaTime);
+
+        // checks if player reached end of grapple
+        if (Vector3.Distance(transform.position, grapplePoint) < 3) {
+            playerState = "move";
+        }
+
+        // cancels grapple if space is pressed
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // carries player grapple momentum over
+            velocity = grappleDirection * grappleSpeed;
+            playerState = "move";
+        }
     }
 }
